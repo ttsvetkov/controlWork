6ff6dbf732f7e5f0bdc3909975b51688fffe44f1
Tsvetkov Anatoly <t-tsvetkov@mail.ru>
2023-07-24 12:50:35 +0300

    добавил class, добавил описание

---
+++ setArray.cs
@@ -0,0 +1,30 @@
+public class setArray
+{
+    public static string[] ConsoleInputArray()
+    {
+        string[] arr = new string[5];
+
+        int flagX = 0; // флаг для парсинга строки координат X
+
+        Console.WriteLine("Введите пять элементов массива через запятую:");
+        // вводим строку
+        string inputX = Console.ReadLine();
+
+        if (inputX != null)
+        {
+            for (int i = 0; i < inputX.Length; i++)
+            {
+                if (inputX[i] == ',') flagX++;
+                if (inputX[i] != ',' && flagX == 0) arr[flagX] += inputX[i];
+                if (inputX[i] != ',' && flagX == 1) arr[flagX] += inputX[i];
+                if (inputX[i] != ',' && flagX == 2) arr[flagX] += inputX[i];
+                if (inputX[i] != ',' && flagX == 3) arr[flagX] += inputX[i];
+                if (inputX[i] != ',' && flagX == 4) arr[flagX] += inputX[i];
+
+            }
+        }
+        else Console.WriteLine($" Массив не получился =) ");
+
+        return arr;
+    }
+}
